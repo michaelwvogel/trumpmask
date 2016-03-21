25469787772814a48ad84b9a6f999ddc5884bcc9
Michael Vogel <michaelwvogel@gmail.com>
2016-03-21 18:28:28 -0400

    added explanatory comments in sketch

--- trumpmask.ino
+++ trumpmask.ino
@@ -1,12 +1,30 @@
-/*
- * This sketch draws from the daphc example included in the WaveHC library for the Adafruit Arduino Waveshield, 
+/* 
+ *  https://github.com/michaelwvogel/trumpmask
+ *  
+ * This sketch draws from the daphc and softVolume examples included in the WaveHC library for the Adafruit Arduino Waveshield, 
  * as well as the adavoice sketch for the Adafruit Microphone Amplifier Breakout.
  * 
  * This sketch detects microphone input to trigger WAV playback, and monitors input from 
  * an IR proximity sensor, manipulating WAV playback volume based on this analog input.
  * 
+ * Hardware requirements:
+ * - Arduino Uno, Duemilanove or Diecimila (not Mega or Leonardo compatible).
+ * - Adafruit Wave Shield
+ * - Speaker attached to Wave Shield output
+ * - Battery for portable use
+ * - Adafruit Microphone Breakout
+ * - SD card
+ * - QRD1114 IR emitter / Phototransistor
+ * - Donald Trump halloween mask
+ * - Baseball cap to hide components 
+ * 
+ * Software requirements:
+ * - WaveHC library for Arduino
+ * - WAV files on FAT-formatted SD card (included here)
+ * 
  * May Trump's life be short.
  */
+ 
 #include <WaveHC.h>
 #include <WaveUtil.h>
 
